36a6a5d21115e20f1672f5b5f083ebbd863a3697
Rustam S. <blakfoh@gmail.com>
2025-11-17 09:17:07 +0300

    [fix] update _layout.tsx - show modal

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -24,6 +24,18 @@ export default function RootLayout() {
 	return (
 		<Stack>
 			<Stack.Screen name='(tabs)' options={{ headerShown: false }} />
+			<Stack.Screen
+				name='pokemonDetails'
+				options={{
+					headerBackButtonDisplayMode: 'minimal',
+					presentation: 'formSheet',
+					// sheetAllowedDetents: [0.3, 0.6, 1],
+					sheetGrabberVisible: true,
+					// headerShown: false,
+					// sheetCornerRadius: 16,
+					fullScreenGestureEnabled: true,
+				}}
+			/>
 		</Stack>
 	)
 }
